7156f7a86acec89a063301e15cbaeb105486b37c
dwijaha <64943225+dwijaha@users.noreply.github.com>
2020-07-15 19:55:29 -0400

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+output "org" {
+  description = "Orginization level information"
+  value = data.google_organization.org
+}
+output "billing" {
+  description = "Billing level information"
+  value = data.google_billing_account.billing
+}
+output "folder" {
+  description = "Department folder level information"
+  value = data.google_active_folder.folder
+}
