3ddd6f9eddec341aa908338f311194fd3a441966
dwijaha <64943225+dwijaha@users.noreply.github.com>
2020-07-15 19:59:26 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "domain" {
+  description = "The domain name associated with our organization"
+  default     = ""
+}
+variable "billing_account_id" {
+  description = "The billing account ID"
+  default     = "012A73-F34CA8-361956" # 
+}
+variable "folder" {
+  description = "The name of the root folder housing our departments projects"
+  default     = ""
+}
