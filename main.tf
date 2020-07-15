eba0b47ee9656a6cbf4f0c32d57040d8349d989e
dwijaha <64943225+dwijaha@users.noreply.github.com>
2020-07-15 19:54:55 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+data "google_organization" "org" {
+  domain = "${var.domain}"
+}
+data "google_billing_account" "billing" {
+  billing_account = "${var.billing_account_id}"
+  open            = true
+}
+data "google_active_folder" "folder" {
+  display_name = "${var.folder}"
+  parent = "${data.google_organization.org.name}"
