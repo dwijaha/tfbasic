355d7df4baae1efdb562bd84da4a9ab3818640fc
dwijaha <64943225+dwijaha@users.noreply.github.com>
2020-07-16 00:19:07 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "domain" {
 }
 variable "billing_account_id" {
   description = "The billing account ID"
-  default     = "012A73-F34CA8-361956" # 
+  default     = "" # 
 }
 # variable "folder" {
 #   description = "The name of the root folder housing our departments projects"
